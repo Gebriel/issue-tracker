3a971867168e81f3c1887b63ebf1f47e14c4dcf0
Gebriel A. Fanta <gebrielabebe@gmail.com>
2024-01-24 12:35:39 +0100

    Update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -37,7 +37,12 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
     const onSubmit = handleSubmit(async (data) => {
         try {
             setSubmitting(true);
-            await axios.post('/api/issues', data);
+            if (issue) {
+                axios.patch('/api/issues/' + issue.id, data);
+            } else {
+                await axios.post('/api/issues', data);
+            }
+
             router.push('/issues');
         } catch (error) {
             setSubmitting(true);
@@ -75,7 +80,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
                 <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
                 <Button disabled={isSubmitting}>
-                    Submit New Issue {isSubmitting && <Spinner />}
+                    {issue ? 'Update Issue' : 'Submit New Issue'}{' '}
+                    {isSubmitting && <Spinner />}
                 </Button>
             </form>
         </div>
