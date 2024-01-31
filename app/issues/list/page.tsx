88fee7f3e5e808105b7360b1f541b26e3e9cfd53
Gebriel A. Fanta <gebrielabebe@gmail.com>
2024-01-31 15:52:00 +0100

    Add Metadata

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -5,6 +5,7 @@ import { Status } from '@prisma/client';
 import IssueActions from './IssueActions';
 import IssueTable, { IssueQuery, columnNames } from './IssueTable';
 import { Flex } from '@radix-ui/themes';
+import { Metadata } from 'next';
 
 interface Props {
     searchParams: IssueQuery;
@@ -47,5 +48,10 @@ const IssuesPage = async ({ searchParams }: Props) => {
 
 export const dynamic = 'force-dynamic';
 //export const revalidate = 60; //revalidate every 60 seconds
+
+export const metadata: Metadata = {
+    title: 'Issue Tracker - Issue List',
+    description: 'View all project issues',
+};
 
 export default IssuesPage;
